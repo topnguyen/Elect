2474343e2d07f30e16c25da8a7babaa64e2f65e0
Top Nguyen <admin@topnguyen.com>
2018-03-15 11:59:38 +0700

    structure project

---
+++ vstemplates/Class/Class.cs
@@ -0,0 +1,31 @@
+﻿#region	License
+//--------------------------------------------------
+// <License>
+//     <Copyright> $year$ © $author$ </Copyright>
+//     <Url> $authorurl$ </Url>
+//     <Author> $username$ </Author>
+//     <Project> $project$ </Project>
+//     <File>
+//         <Name> $safeitemname$.cs </Name>
+//         <Created> $time$ </Created>
+//         <Key> $guid10$ </Key>
+//     </File>
+//     <Summary>
+//         $safeitemname$.cs is a part of $project$
+//     </Summary>
+// <License>
+//--------------------------------------------------
+#endregion License
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+	
+namespace $rootnamespace$
+{
+    public class $safeitemname$
+    {
+    }
+}
