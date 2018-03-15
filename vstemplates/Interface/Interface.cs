3dac5b0ef0f3b95b6b80af9676fe4b161f318195
Top <topnguyen92@gmail.com>
2018-03-15 11:59:38 +0700

    structure project

---
+++ vstemplates/Interface/Interface.cs
@@ -0,0 +1,30 @@
+﻿#region	License
+//--------------------------------------------------
+// <License>
+//     <Copyright> $year$ © $author$ </Copyright>
+//     <Url> $authorurl$ </Url>
+//     <Author> $username$ </Author>
+//     <Project> $project$ </Project>
+//     <File>
+//         <Name> $safeitemname$.cs </Name>
+//         <Created> $time$ </Created>
+//         <Key> $guid10$ </Key>
+//     </File>
+//     <Summary>
+//         $safeitemname$.cs is a part of $project$
+//     </Summary>
+// <License>
+//--------------------------------------------------
+#endregion License
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+
+namespace $rootnamespace$
+{
+    public interface $safeitemname$
+    {
+    }
+}
