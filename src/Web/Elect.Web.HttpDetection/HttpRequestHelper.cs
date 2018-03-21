599b33f4106c4500caf4e006776203d0fdfe1d9e
Top Nguyen <admin@topnguyen.com>
2018-03-21 20:51:32 +0700

    HttpDetection Initial

---
+++ src/Web/Elect.Web.HttpDetection/HttpRequestHelper.cs
@@ -0,0 +1,308 @@
+﻿#region	License
+//--------------------------------------------------
+// <License>
+//     <Copyright> 2018 © Top Nguyen </Copyright>
+//     <Url> http://topnguyen.net/ </Url>
+//     <Author> Top </Author>
+//     <Project> Elect </Project>
+//     <File>
+//         <Name> HttpRequestHelper.cs </Name>
+//         <Created> 21/03/2018 8:35:12 PM </Created>
+//         <Key> 61ca0fd2-deba-4af6-8d54-07ce8d993e65 </Key>
+//     </File>
+//     <Summary>
+//         HttpRequestHelper.cs is a part of Elect
+//     </Summary>
+// <License>
+//--------------------------------------------------
+#endregion License
+
+using Elect.Core.LinqUtils;
+using Elect.Data.IO;
+using Elect.Web.HttpDetection.Models;
+using Elect.Web.HttpUtils;
+using Elect.Web.Models;
+using MaxMind.GeoIP2;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Linq;
+
+namespace Elect.Web.HttpDetection
+{
+    public class HttpRequestHelper
+    {
+        // Marker
+        public static string GetMarkerFullInfo(HttpRequest request)
+        {
+            var agent = GetUserAgent(request);
+
+            if (string.IsNullOrWhiteSpace(agent))
+            {
+                return null;
+            }
+
+            int iEnd = agent.IndexOf('(');
+
+            if (iEnd < 0)
+            {
+                return null;
+            }
+
+            string markerFullInfo = agent.Substring(0, iEnd)?.Trim();
+
+            return markerFullInfo;
+        }
+
+        public static string GetMarkerName(HttpRequest request)
+        {
+            string markerName = GetMarkerFullInfo(request)?.Split('/').FirstOrDefault()?.Trim();
+
+            return markerName;
+        }
+
+        public static string GetMarkerVersion(HttpRequest request)
+        {
+            string markerVersion = GetMarkerFullInfo(request)?.Split('/').LastOrDefault()?.Trim();
+
+            return markerVersion;
+        }
+
+        // OS
+        public static string GetOsFullInfo(HttpRequest request)
+        {
+            var agent = GetUserAgent(request);
+
+            if (string.IsNullOrWhiteSpace(agent))
+            {
+                return null;
+            }
+
+            int iStart = agent.IndexOf('(') + 1;
+
+            int iEnd = agent.IndexOf(')') - iStart;
+
+            if (iEnd < 0)
+            {
+                return null;
+            }
+
+            string osFullInfo = agent.Substring(iStart, iEnd)?.Trim();
+
+            return osFullInfo;
+        }
+
+        public static string GetOsName(HttpRequest request)
+        {
+            string osName = GetOsFullInfo(request)?.Split(';').FirstOrDefault()?.Trim();
+
+            return osName;
+        }
+
+        public static string GetOsVersion(HttpRequest request)
+        {
+            var info = GetOsFullInfo(request)?.Split(';');
+
+            string osVersion = null;
+
+            if (info != null && info.Length > 0)
+            {
+                int i = 1;
+
+                while (i <= info.Length && (osVersion == null || osVersion.ToLowerInvariant() == "u"))
+                {
+                    osVersion = info[i];
+
+                    i++;
+                }
+            }
+
+            return osVersion;
+        }
+
+        // Engine
+        public static string GetEngineFullInfo(HttpRequest request)
+        {
+            var agent = GetUserAgent(request);
+
+            if (string.IsNullOrWhiteSpace(agent))
+            {
+                return null;
+            }
+
+            int iStart = agent.IndexOf(')') + 1;
+
+            string engineFullInfo = agent.Substring(iStart)?.Trim();
+
+            if (string.IsNullOrWhiteSpace(engineFullInfo))
+            {
+                return null;
+            }
+
+            int iEnd = engineFullInfo.IndexOf(' ');
+
+            if (iEnd < 0)
+            {
+                return null;
+            }
+
+            engineFullInfo = engineFullInfo.Substring(0, iEnd);
+
+            return engineFullInfo;
+        }
+
+        public static string GetEngineName(HttpRequest request)
+        {
+            string engineName = GetEngineFullInfo(request)?.Split('/').FirstOrDefault()?.Trim();
+
+            // Standardize engine name
+            const string webKitStandardName = "WebKit";
+
+            engineName = engineName?.EndsWith(webKitStandardName) == true ? webKitStandardName : engineName;
+
+            return engineName;
+        }
+
+        public static string GetEngineVersion(HttpRequest request)
+        {
+            string engineName = GetEngineFullInfo(request)?.Split('/').LastOrDefault()?.Trim();
+
+            return engineName;
+        }
+
+        // Browser
+
+        /// <summary>
+        ///     Get browser info in user agent 
+        /// </summary>
+        /// <param name="request">      </param>
+        /// <param name="isStandardize"> will exclude "version", "mobile" from browser info </param>
+        /// <returns></returns>
+        public static string GetBrowserFullInfo(HttpRequest request, bool isStandardize = true)
+        {
+            var agent = GetUserAgent(request);
+
+            if (string.IsNullOrWhiteSpace(agent))
+            {
+                return null;
+            }
+
+            int iStart = agent.LastIndexOf(')') + 1;
+
+            string browserFullInfo = agent.Substring(iStart)?.Trim();
+
+            // Filters
+            if (!string.IsNullOrWhiteSpace(browserFullInfo))
+            {
+                var listExclude = new[] { "version", "mobile" };
+
+                var browserInfo = browserFullInfo.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+
+                if (isStandardize)
+                {
+                    browserInfo = browserInfo.RemoveWhere(x => listExclude.Any(y => x.ToLowerInvariant().Contains(y))).ToList();
+                }
+
+                browserFullInfo = string.Join(", ", browserInfo);
+            }
+
+            return browserFullInfo;
+        }
+
+        public static string GetBrowserName(HttpRequest request)
+        {
+            string browserName = GetBrowserFullInfo(request)?.Split(',').FirstOrDefault()?.Split('/').FirstOrDefault()?.Trim();
+
+            return browserName;
+        }
+
+        public static string GetBrowserVersion(HttpRequest request)
+        {
+            string browserFullInfo = GetBrowserFullInfo(request, false);
+
+            // Filters
+            if (string.IsNullOrWhiteSpace(browserFullInfo))
+            {
+                return null;
+            }
+
+            var browserInfo = browserFullInfo.Split(' ').ToList();
+
+            var browserVersion = browserInfo.FirstOrDefault(x => x.ToLowerInvariant().Contains("version"))?.Split('/').LastOrDefault()?.Trim().Trim(',');
+
+            if (string.IsNullOrWhiteSpace(browserVersion))
+            {
+                browserVersion = GetBrowserFullInfo(request)?.Split(',').FirstOrDefault()?.Split('/').LastOrDefault()?.Trim();
+            }
+
+            return browserVersion;
+        }
+
+        public static string GetUserAgent(HttpRequest request)
+        {
+            var agent = request?.Headers[HeaderKey.UserAgent].ToString();
+
+            return agent;
+        }
+
+        public static DeviceModel GetDeviceInformation(HttpRequest request, DeviceModel device)
+        {
+            string geoDbRelativePath = Path.Combine(nameof(HttpUtils), nameof(HttpDetection), "GeoCity.mmdb");
+
+            string geoDbAbsolutePath = PathHelper.GetFullPath(geoDbRelativePath);
+
+            if (!File.Exists(geoDbAbsolutePath))
+            {
+                // Try to get folder in executed assembly
+                geoDbAbsolutePath = PathHelper.GetFullPath(geoDbRelativePath);
+            }
+
+            if (!File.Exists(geoDbAbsolutePath))
+            {
+                return null;
+            }
+
+            using (var reader = new DatabaseReader(geoDbAbsolutePath))
+            {
+                var ipAddress = request.GetIpAddress();
+
+                if (!reader.TryCity(ipAddress, out var city))
+                {
+                    return device;
+                }
+
+                if (city == null)
+                {
+                    return device;
+                }
+
+                device.IpAddress = city.Traits.IPAddress;
+
+                // City
+                device.CityName = city.City.Names.TryGetValue("en", out var cityName) ? cityName : city.City.Name;
+                device.CityGeoNameId = city.City.GeoNameId;
+
+                // Country
+                device.CountryName = city.Country.Names.TryGetValue("en", out var countryName) ? countryName : city.Country.Name;
+                device.CountryGeoNameId = city.Country.GeoNameId;
+                device.CountryIsoCode = city.Country.IsoCode;
+
+                // Continent
+                device.ContinentName = city.Continent.Names.TryGetValue("en", out var continentName) ? continentName : city.Continent.Name;
+                device.ContinentGeoNameId = city.Continent.GeoNameId;
+                device.ContinentCode = city.Continent.Code;
+
+                // Location
+                device.Latitude = city.Location.Latitude;
+                device.Longitude = city.Location.Longitude;
+                device.AccuracyRadius = city.Location.AccuracyRadius;
+
+                device.PostalCode = city.Postal.Code;
+
+                // Time Zone
+                device.TimeZone = city.Location.TimeZone;
+
+                return device;
+            }
+        }
+    }
+}
---
+++ src/Web/Elect.Web/Middlewares/MinResponseMiddleware/IServiceCollectionExtensions.cs
@@ -0,0 +1,84 @@
+﻿#region	License
+//--------------------------------------------------
+// <License>
+//     <Copyright> 2018 © Top Nguyen </Copyright>
+//     <Url> http://topnguyen.net/ </Url>
+//     <Author> Top </Author>
+//     <Project> Elect </Project>
+//     <File>
+//         <Name> IServiceCollectionExtensions.cs </Name>
+//         <Created> 21/03/2018 8:06:58 PM </Created>
+//         <Key> d6a913c0-195a-4b62-99b5-455bcf304151 </Key>
+//     </File>
+//     <Summary>
+//         IServiceCollectionExtensions.cs is a part of Elect
+//     </Summary>
+// <License>
+//--------------------------------------------------
+#endregion License
+
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.IO.Compression;
+using WebMarkupMin.AspNet.Common.Compressors;
+using WebMarkupMin.AspNetCore2;
+using WebMarkupMin.NUglify;
+
+namespace Elect.Web.Middlewares.MinResponseMiddleware
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     Mini and compress HTML, XML, CSS, JS 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddElectMinResponse(this IServiceCollection services)
+        {
+            services
+                // Global
+                .AddWebMarkupMin(options =>
+                {
+                    options.MaxResponseSize = -1; // Allow min all size
+                    options.AllowMinificationInDevelopmentEnvironment = true;
+                    options.AllowCompressionInDevelopmentEnvironment = true;
+                    options.DisablePoweredByHttpHeaders = false;
+                    options.DisableCompression = false;
+                    options.DisableMinification = false;
+                })
+                // HTML, CSS, JS Mini
+                .AddHtmlMinification(options =>
+                {
+                    options.MinificationSettings.MinifyEmbeddedCssCode = true;
+                    options.MinificationSettings.RemoveRedundantAttributes = false;
+                    options.MinificationSettings.RemoveHttpProtocolFromAttributes = false;
+                    options.MinificationSettings.RemoveHttpsProtocolFromAttributes = false;
+                    options.MinificationSettings.RemoveOptionalEndTags = false; // Important, Don't enable/true this
+                    options.CssMinifierFactory = new NUglifyCssMinifierFactory();
+                    options.JsMinifierFactory = new NUglifyJsMinifierFactory();
+                })
+                // XML Mini
+                .AddXmlMinification(options =>
+                {
+                    options.MinificationSettings.CollapseTagsWithoutContent = true;
+                })
+                // Compress
+                .AddHttpCompression(options =>
+                {
+                    options.CompressorFactories = new List<ICompressorFactory>
+                    {
+                        new DeflateCompressorFactory(new DeflateCompressionSettings
+                        {
+                            Level = CompressionLevel.Fastest
+                        }),
+                        new GZipCompressorFactory(new GZipCompressionSettings
+                        {
+                            Level = CompressionLevel.Fastest
+                        })
+                    };
+                });
+
+            return services;
+        }
+    }
+}
