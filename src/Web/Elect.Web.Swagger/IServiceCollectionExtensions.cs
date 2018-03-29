c766fbdc8244eecf37ba7aba6c8b8894333364fc
Top <topnguyen92@gmail.com>
2018-03-29 18:11:59 +0700

    add service for swagger

---
+++ src/Web/Elect.Web.Swagger/IServiceCollectionExtensions.cs
@@ -0,0 +1,114 @@
+﻿using Elect.Core.ActionUtils;
+using Elect.Core.Attributes;
+using Elect.Web.Swagger.IDocumentFilters;
+using Elect.Web.Swagger.Models;
+using Elect.Web.Swagger.Utils;
+using Elect.Web.Swagger.Utils.SwaggerGenOptionsUtils;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Reflection;
+
+namespace Elect.Web.Swagger
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddElectSwagger(this IServiceCollection services)
+        {
+            return services.AddElectSwagger(_ => { });
+        }
+
+        public static IServiceCollection AddElectSwagger(this IServiceCollection services, [NotNull] ElectSwaggerOptions configuration)
+        {
+            return services.AddElectSwagger(_ =>
+            {
+                _.SwaggerRoutePrefix = configuration.SwaggerRoutePrefix;
+                _.SwaggerName = configuration.SwaggerName;
+                _.Url = configuration.Url;
+                _.JsonViewerUrl = configuration.JsonViewerUrl;
+                _.Title = configuration.Title;
+                _.Version = configuration.Version;
+                _.AccessKey = configuration.AccessKey;
+                _.UnAuthorizeMessage = configuration.UnAuthorizeMessage;
+                _.AuthTokenType = configuration.AuthTokenType;
+                _.IsFullSchemaForType = configuration.IsFullSchemaForType;
+                _.IsDescribeAllEnumsAsString = configuration.IsDescribeAllEnumsAsString;
+                _.IsDescribeAllParametersInCamelCase = configuration.IsDescribeAllParametersInCamelCase;
+                _.AuthorName = configuration.AuthorName;
+                _.AuthorEmail = configuration.AuthorEmail;
+                _.AuthorWebsite = configuration.AuthorWebsite;
+            });
+        }
+
+        public static IServiceCollection AddElectSwagger(this IServiceCollection services, [NotNull] Action<ElectSwaggerOptions> configuration)
+        {
+            services.Configure(configuration);
+
+            var options = configuration.GetValue();
+
+            // Update File Content base on Configuration
+
+            SwaggerHelper.UpdateApiDocFileContent(options.Title, options.SwaggerRoutePrefix, options.AuthTokenType, options.JsonViewerUrl);
+
+            SwaggerHelper.UpdateApiJsonViewerFileContent(options.Title);
+
+            // Config Swagger
+
+            services.AddSwaggerGen(swaggerGenOptions =>
+            {
+                // Doc Info
+                swaggerGenOptions.SwaggerDoc(options.Version, new Info
+                {
+                    Title = options.Title,
+                    Version = options.Version,
+                    Contact = !string.IsNullOrWhiteSpace(options.AuthorName)
+                              && !string.IsNullOrWhiteSpace(options.AuthorWebsite)
+                              && !string.IsNullOrWhiteSpace(options.AuthorEmail)
+                        ? new Contact
+                        {
+                            Name = options.AuthorName,
+                            Url = options.AuthorWebsite,
+                            Email = options.AuthorEmail
+                        }
+                        : null
+                });
+
+                // XML
+                swaggerGenOptions.IncludeXmlCommentsIfExists(Assembly.GetCallingAssembly());
+
+                // Filers
+                swaggerGenOptions.DocumentFilter<HideInApiDocDocumentFilter>();
+
+                swaggerGenOptions.IgnoreObsoleteProperties();
+
+                swaggerGenOptions.IgnoreObsoleteActions();
+
+                // Type / Properties
+                if (options.IsFullSchemaForType)
+                {
+                    swaggerGenOptions.CustomSchemaIds(type => type.FullName);
+                }
+
+                if (options.IsDescribeAllParametersInCamelCase)
+                {
+                    swaggerGenOptions.DescribeAllParametersInCamelCase();
+                }
+
+                if (options.IsDescribeAllEnumsAsString)
+                {
+                    swaggerGenOptions.DescribeAllEnumsAsStrings();
+
+                    if (options.IsDescribeAllParametersInCamelCase)
+                    {
+                        swaggerGenOptions.DescribeStringEnumsInCamelCase();
+                    }
+                }
+
+                // Order
+                swaggerGenOptions.OrderActionsBy(apiDesc => $"[{apiDesc.HttpMethod}]{apiDesc.RelativePath}");
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/Web/Elect.Web.Swagger/apidoc/lib/Readme.md
@@ -0,0 +1,35 @@
+﻿Original List
+
+```markup
+<script src="@AssetPath/lib/jquery-1.8.0.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/jquery.slideto.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/jquery.wiggle.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/jquery.ba-bbq.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/handlebars-2.0.0.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/underscore-min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/backbone-min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/swagger-ui.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/highlight.7.3.pack.js" type='text/javascript'></script>
+<script src="@AssetPath/lib/marked.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/swagger-oauth.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/bootstrap.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/clipboard.min.js"></script>
+```
+
+Minifiled List
+
+```markup
+<script src="@AssetPath/lib/jquery-1.8.0.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/jquery.slideto.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/jquery.wiggle.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/jquery.ba-bbq.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/handlebars-2.0.0.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/underscore-min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/backbone-min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/swagger-ui.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/highlight.7.3.pack.min.js" type='text/javascript'></script>
+<script src="@AssetPath/lib/marked.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/swagger-oauth.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/bootstrap.min.js" type="text/javascript"></script>
+<script src="@AssetPath/lib/clipboard.min.js"></script>
+```
