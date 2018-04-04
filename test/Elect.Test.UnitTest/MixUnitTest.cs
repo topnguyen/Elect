acc8f28cc00cd151ffdb3bdb8e00e067b5c1817b
Top Nguyen <admin@topnguyen.com>
2018-04-04 20:26:38 +0700

    update Data.IO version

--- test/Elect.Test.UnitTest/MixUnitTest.cs
+++ test/Elect.Test.UnitTest/MixUnitTest.cs
@@ -18,7 +18,9 @@
 #endregion License
 
 using Elect.Data.IO.ImageUtils.CompressUtils;
+using Elect.Data.IO.ImageUtils.ResizeUtils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace Elect.Test.UnitTest
 {
@@ -31,6 +33,14 @@ namespace Elect.Test.UnitTest
             ImageCompressor.Compress(@"D:\Collection Image\1.jpg", @"D:\_Temp\1.jpg");
             ImageCompressor.Compress(@"D:\Collection Image\toptal-blog-1_B.png", @"D:\_Temp\toptal-blog-1_B.png");
             ImageCompressor.Compress(@"D:\Collection Image\toptal-blog-image-1387828457247.gif", @"D:\_Temp\toptal-blog-image-1387828457247.gif");
+        }
+
+        [TestMethod]
+        public void ResizeUnitTest()
+        {
+            var resizedImage = ImageResizeHelper.Resize(File.ReadAllBytes(@"D:\Collection Image\1.jpg"), 100, 100);
+
+            File.WriteAllBytes(@"D:\_Temp\1.jpg", resizedImage);
         }
     }
 }
