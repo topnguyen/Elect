7b283100ff265a2fcb517f5e4026560b9b87f481
Top Nguyen <admin@topnguyen.com>
2018-04-10 17:38:43 +0700

    update datatable version

--- src/Web/Elect.Web.DataTable/Attributes/DataTableBaseAttribute.cs
+++ src/Web/Elect.Web.DataTable/Attributes/DataTableBaseAttribute.cs
@@ -23,6 +23,7 @@ using System.Reflection;
 
 namespace Elect.Web.DataTable.Attributes
 {
+    [AttributeUsage(AttributeTargets.Property)]
     public abstract class DataTableBaseAttribute : Attribute
     {
         public abstract void ApplyTo(ColumnModel columnModel, PropertyInfo propertyInfo);
