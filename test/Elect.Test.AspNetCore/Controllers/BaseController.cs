7f400aeea48b2b8d74928d9f6a46ce644a7c72ca
Top Nguyen <admin@topnguyen.com>
2018-04-19 00:05:13 +0700

    improve test

---
+++ test/Elect.Test.AspNetCore/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Elect.Web.Swagger.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elect.Test.AspNetCore.Controllers
+{
+    [ShowInApiDoc]
+    public class BaseController : Controller
+    {
+    }
+}
