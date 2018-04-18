955ae67bdc81aac4b2d09da182158818bfc16e21
Top <topnguyen92@gmail.com>
2018-04-19 00:05:13 +0700

    improve test

--- test/Elect.Test.AspNetCore/Controllers/TestApiController.cs
+++ test/Elect.Test.AspNetCore/Controllers/TestApiController.cs
@@ -1,33 +1,18 @@
 ﻿using Elect.Web.HttpDetection;
-using Elect.Web.Swagger.Attributes;
-using Elect.Web.Swagger.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Elect.Test.AspNetCore.Controllers
 {
-    /// <summary>
-    ///     Test Controller 
-    /// </summary>
-    [Authorize]
-    [Route("[controller]/[action]")]
-    public class TestApiController : Controller
+    public class TestApiController : BaseController
     {
         /// <summary>
         ///     Get device info 
         /// </summary>
         /// <returns></returns>
-        [HttpPost]
-        [AllowAnonymous]
-        [ShowInApiDoc]
-        [ApiParameter("test_header", In = ParameterInType.Header, Description = "test header field")]
-        [ApiParameter("test_query", In = ParameterInType.Query, Description = "test query field")]
-        [ApiParameter("test_route", In = ParameterInType.Route, Description = "test route field")]
-        [Route("/{test_route}")]
+        [HttpGet]
+        [Route("device-info")]
         public IActionResult Device()
         {
-            var request = Request;
-
             var device = HttpContext.Request.GetDeviceInformation();
 
             return Ok(device);
