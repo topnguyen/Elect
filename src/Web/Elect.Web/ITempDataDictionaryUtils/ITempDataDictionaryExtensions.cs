930fbe934a930812ac283b0e3c8ab0b865a0e492
Top <topnguyen92@gmail.com>
2018-04-19 01:18:26 +0700

    add tempdata

---
+++ src/Web/Elect.Web/ITempDataDictionaryUtils/ITempDataDictionaryExtensions.cs
@@ -0,0 +1,36 @@
+﻿#region	License
+//--------------------------------------------------
+// <License>
+//     <Copyright> 2018 © Top Nguyen </Copyright>
+//     <Url> http://topnguyen.net/ </Url>
+//     <Author> Top </Author>
+//     <Project> Elect </Project>
+//     <File>
+//         <Name> ITempDataDictionaryExtensions.cs </Name>
+//         <Created> 19/04/2018 1:08:26 AM </Created>
+//         <Key> 9f4cab38-a4fa-402d-a659-80a6f61caf18 </Key>
+//     </File>
+//     <Summary>
+//         ITempDataDictionaryExtensions.cs is a part of Elect
+//     </Summary>
+// <License>
+//--------------------------------------------------
+#endregion License
+
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Elect.Web.ITempDataDictionaryUtils
+{
+    public static class ITempDataDictionaryExtensions
+    {
+        public static void Set<T>(this ITempDataDictionary tempData, string key, T value) where T : class
+        {
+            ITempDataDictionaryHelper.Set<T>(tempData, key, value);
+        }
+
+        public static T Get<T>(this ITempDataDictionary tempData, string key) where T : class
+        {
+            return ITempDataDictionaryHelper.Get<T>(tempData, key);
+        }
+    }
+}
---
+++ src/Web/Elect.Web/ITempDataDictionaryUtils/ITempDataDictionaryHelper.cs
@@ -0,0 +1,42 @@
+﻿#region	License
+//--------------------------------------------------
+// <License>
+//     <Copyright> 2018 © Top Nguyen </Copyright>
+//     <Url> http://topnguyen.net/ </Url>
+//     <Author> Top </Author>
+//     <Project> Elect </Project>
+//     <File>
+//         <Name> ITempDataDictionaryHelper.cs </Name>
+//         <Created> 19/04/2018 1:08:58 AM </Created>
+//         <Key> ddd4e9cd-7eed-4763-98ec-9837a2f6ab9c </Key>
+//     </File>
+//     <Summary>
+//         ITempDataDictionaryHelper.cs is a part of Elect
+//     </Summary>
+// <License>
+//--------------------------------------------------
+#endregion License
+
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+
+namespace Elect.Web.ITempDataDictionaryUtils
+{
+    public class ITempDataDictionaryHelper
+    {
+        public static void Set<T>(ITempDataDictionary tempData, string key, T value) where T : class
+        {
+            tempData[key] = JsonConvert.SerializeObject(value);
+        }
+
+        public static T Get<T>(ITempDataDictionary tempData, string key) where T : class
+        {
+            if (tempData.TryGetValue(key, out var o))
+            {
+                return o == null ? null : JsonConvert.DeserializeObject<T>((string)o);
+            }
+
+            return null;
+        }
+    }
+}
