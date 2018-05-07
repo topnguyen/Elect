672e364f56736b35352b5861e8283094a013c3f5
Top <topnguyen92@gmail.com>
2018-05-07 16:49:27 +0700

    update auto mapper readme

---
+++ samples/Mapper/Elect.Sample.Mapper.AutoMapper/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elect.Sample.Mapper.AutoMapper
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost webHost = BuildWebHost(args);
+
+            OnAppStart(webHost);
+
+            webHost.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var webHostBuilder = WebHost.CreateDefaultBuilder(args);
+
+            webHostBuilder.UseStartup<Startup>();
+
+            var webHost = webHostBuilder.Build();
+
+            return webHost;
+        }
+
+        private static void OnAppStart(IWebHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+            }
+        }
+    }
+}
