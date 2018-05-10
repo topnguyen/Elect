f6ea76c647f9cd5bc80b2d829462a18cd0a652ee
Top Nguyen <admin@topnguyen.com>
2018-05-10 15:46:32 +0700

    Swagger Document without Sample

---
+++ samples/Web/Elect.Sample.Web.Swagger/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Elect.Sample.Web.Swagger.Controllers
+{
+    [ShowInApiDoc]
+    public class HomeController : Controller
+    {
+        public IActionResult GetUser()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ samples/Web/Elect.Sample.Web.Swagger/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Elect.Sample.Web.Swagger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost webHost = BuildWebHost(args);
+
+            webHost.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var webHostBuilder = WebHost.CreateDefaultBuilder(args);
+
+            webHostBuilder.UseStartup<Startup>();
+
+            var webHost = webHostBuilder.Build();
+
+            return webHost;
+        }
+    }
+}
