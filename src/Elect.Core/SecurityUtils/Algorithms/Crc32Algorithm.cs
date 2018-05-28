8f7495109f8a2ceddfd48540e853cf1f4763e1b9
Top Nguyễn <topnguyen92@gmail.com>
2018-05-29 00:29:33 +0700

    Support Id generator, udpate libs and upgrade all nuget to base version 1.0.8

---
+++ src/Elect.Core/SecurityUtils/Algorithms/Crc32Algorithm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Elect.Core.SecurityUtils.Algorithms
+{
+    public sealed class Crc32 : HashAlgorithm
+    {
+        #region Properties
+
+        public const uint DefaultPolynomial = 0xedb88320u;
+        public const uint DefaultSeed = 0xffffffffu;
+        public override int HashSize => 32;
+
+        #endregion
+
+        #region Fields
+
+        private static uint[] _defaultTable;
+        private readonly uint _seed;
+        private readonly uint[] _table;
+        private uint _hash;
+
+        #endregion
+
+        public Crc32() : this(DefaultPolynomial, DefaultSeed)
+        {
+        }
+
+        public Crc32(uint polynomial, uint seed)
+        {
+            _table = InitializeTable(polynomial);
+
+            _seed = _hash = seed;
+        }
+
+        #region Override
+
+        public override void Initialize()
+        {
+            _hash = _seed;
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            _hash = CalculateHash(_table, _hash, array, ibStart, cbSize);
+        }
+
+        protected override byte[] HashFinal()
+        {
+            var hashBuffer = UintToBigEndianBytes(~_hash);
+
+            HashValue = hashBuffer;
+
+            return hashBuffer;
+        }
+
+        #endregion
+
+        #region Private Helper
+
+        private static uint[] InitializeTable(uint polynomial)
+        {
+            if (polynomial == DefaultPolynomial && _defaultTable != null)
+            {
+                return _defaultTable;
+            }
+
+            var createTable = new uint[256];
+
+            for (var i = 0; i < 256; i++)
+            {
+                var entry = (uint) i;
+                for (var j = 0; j < 8; j++)
+                    if ((entry & 1) == 1)
+                    {
+                        entry = (entry >> 1) ^ polynomial;
+                    }
+                    else
+                    {
+                        entry = entry >> 1;
+                    }
+
+                createTable[i] = entry;
+            }
+
+            if (polynomial == DefaultPolynomial)
+            {
+                _defaultTable = createTable;
+            }
+
+            return createTable;
+        }
+
+        private static uint CalculateHash(uint[] table, uint seed, IList<byte> buffer, int start, int size)
+        {
+            var hash = seed;
+            for (var i = start; i < start + size; i++)
+                hash = (hash >> 8) ^ table[buffer[i] ^ hash & 0xff];
+            return hash;
+        }
+
+        private static byte[] UintToBigEndianBytes(uint @uint)
+        {
+            var result = BitConverter.GetBytes(@uint);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(result);
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Elect.Core/StringUtils/IdHelper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Elect.Core.StringUtils
+{
+    public static class IdHelper
+    {
+        #region Short Id Generator
+
+        public static string ToShortString(uint id)
+        {
+            var idBytes = BitConverter.GetBytes(id);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(idBytes);
+            }
+
+            var idString = Safe32Encoding.EncodeBytes(idBytes);
+
+            return idString;
+        }
+
+        public static uint FromShortString(string idString)
+        {
+            var idBytes = Safe32Encoding.DecodeBytes(idString);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(idBytes);
+            }
+
+            var id = BitConverter.ToUInt32(idBytes, 0);
+
+            return id;
+        }
+
+        public static bool TryFromShortString(string idString, out uint id)
+        {
+            try
+            {
+                id = FromShortString(idString);
+
+                return true;
+            }
+            catch
+            {
+                id = 0;
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Long Id Generator
+
+        public static string ToString(ulong id)
+        {
+            var idBytes = BitConverter.GetBytes(id);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(idBytes);
+            }
+
+            var idString = Safe64Encoding.EncodeBytes(idBytes);
+
+            return idString;
+        }
+
+        public static ulong FromString(string idString)
+        {
+            var idBytes = Safe64Encoding.DecodeBytes(idString);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(idBytes);
+            }
+
+            var id = BitConverter.ToUInt64(idBytes, 0);
+
+            return id;
+        }
+
+        public static bool TryFromString(string idString, out ulong id)
+        {
+            try
+            {
+                id = FromString(idString);
+
+                return true;
+            }
+            catch
+            {
+                id = 0;
+
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
