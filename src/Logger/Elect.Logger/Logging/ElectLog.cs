4afaec2942c063b73c91e25c0890b82d0494d7cf
Top Nguyễn <topnguyen92@gmail.com>
2018-06-12 21:16:40 +0700

    cencor credit card

--- src/Logger/Elect.Logger/Logging/ElectLog.cs
+++ src/Logger/Elect.Logger/Logging/ElectLog.cs
@@ -1,37 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using Elect.Core.ObjUtils;
 using Elect.Logger.Models.Logging;
+using Elect.Logger.Utils;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace Elect.Logger.Logging
 {
     public class ElectLog : ElectMessageQueue<LogModel>
     {
+        private static ElectLog _instance;
+
+        public static ElectLog Instance => _instance = _instance ?? new ElectLog();
+
         public Func<LogModel, LogModel> BeforeLog { get; set; }
 
-        public LogModel Capture(string message, HttpContext httpContent = null)
+        public LogModel Capture(string message, LogType type = LogType.Error, HttpContext httpContent = null)
         {
-            var log = new LogModel(message, httpContent);
+            var log = new LogModel(message, httpContent) {Type = type};
 
             return Capture(log);
         }
 
-        public LogModel Capture(Exception exception, HttpContext httpContent = null)
+        public LogModel Capture(Exception exception, LogType type = LogType.Error, HttpContext httpContent = null)
         {
-            var log = new LogModel(exception, httpContent);
+            var log = new LogModel(exception, httpContent) {Type = type};
 
             return Capture(log);
         }
 
-        public LogModel Capture(object obj, HttpContext httpContent = null)
+        public LogModel Capture(object obj, LogType type = LogType.Error, HttpContext httpContent = null)
         {
-            var log = new LogModel(obj, httpContent);
+            var log = new LogModel(obj, httpContent) {Type = type};
 
             return Capture(log);
         }
 
         private LogModel Capture(LogModel log)
         {
+            // Convert to Json string for Filter purpose
+            var logJsonStr = log.ToJsonString();
+
+            // Filter Credit Card
+            logJsonStr = CreditCardHelper.Filter(logJsonStr);
+
+            // Update log by filtered info
+            log = JsonConvert.DeserializeObject<LogModel>(logJsonStr);
+
             if (BeforeLog != null)
             {
                 log = BeforeLog(log);
