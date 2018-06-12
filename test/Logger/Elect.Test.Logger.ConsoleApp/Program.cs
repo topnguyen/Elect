09e73d2925ba0480d77aa315c41dcdd4fc65d7ce
Top Nguyễn <topnguyen92@gmail.com>
2018-06-12 21:43:18 +0700

    Add console test app

---
+++ test/Logger/Elect.Test.Logger.ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Elect.Logger.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elect.Test.Logger.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddElectLog();
+
+            var provider = services.BuildServiceProvider();
+
+            using (var scoped = provider.CreateScope())
+            {
+                var electLog = scoped.ServiceProvider.GetService<IElectLog>();
+                
+                electLog.BeforeLog = (logInfo) =>
+                {
+                    var log = logInfo;
+                    return logInfo;
+                };
+            
+                electLog.Capture("message credit cart is 378282246310005");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
