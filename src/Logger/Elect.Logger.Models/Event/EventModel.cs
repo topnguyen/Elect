ee45f9b934c996639b4f204edf276a7d68bdab4d
Top Nguyễn <topnguyen92@gmail.com>
2018-06-12 19:58:19 +0700

    Improve event model

--- src/Logger/Elect.Logger.Models/Event/EventModel.cs
+++ src/Logger/Elect.Logger.Models/Event/EventModel.cs
@@ -32,7 +32,27 @@ namespace Elect.Logger.Models.Event
         /// <summary>
         ///     Canbe a Page/Screen/Popup or Element in a Screen
         /// </summary>
-        public string Element { get; set; }
+        public string ElementName { get; set; }
+
+        /// <summary>
+        ///     Descart Latitude of element in pixel
+        /// </summary>
+        public double ElementDescartLatPx { get; set; }
+
+        /// <summary>
+        ///     Descart Longitude of element in pixel
+        /// </summary>
+        public double ElementDescartLngPx { get; set; }
+
+        /// <summary>
+        ///     Screen width in pixel
+        /// </summary>
+        public double ScreenWidthPx { get; set; }
+
+        /// <summary>
+        ///     Screen height in pixel
+        /// </summary>
+        public double ScreenHeightPx { get; set; }
 
         public Dictionary<string, object> AdditionalData { get; set; } = new Dictionary<string, object>();
 
