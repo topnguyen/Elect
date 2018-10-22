9e28c0325108229428bb0b6fed5f5f32dea555fd
Top Nguyen <admin@topnguyen.com>
2018-10-22 23:49:39 +0700

    update libs

--- src/Logger/Elect.Logger/Logging/IElectLog.cs
+++ src/Logger/Elect.Logger/Logging/IElectLog.cs
@@ -6,6 +6,16 @@ namespace Elect.Logger.Logging
 {
     public interface IElectLog
     {
+        /// <summary>
+        ///     Modify log info or do some logic before Elect write log.
+        /// </summary>
+        Func<LogModel, LogModel> BeforeLog { get; set; }
+
+        /// <summary>
+        ///     Modify log info or do some logic after Elect write log.
+        /// </summary>
+        Func<LogModel, LogModel> AfterLog { get; set; }
+        
         LogModel Capture(string message, LogType type = LogType.Error, HttpContext httpContent = null);
 
         LogModel Capture(Exception exception, LogType type = LogType.Error, HttpContext httpContent = null);
