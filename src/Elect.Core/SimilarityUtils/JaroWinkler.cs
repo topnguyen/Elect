ad2f65a7bd714a140f6ecb6e3882c2f8e6a605ba
Top Nguyen <admin@topnguyen.com>
2018-11-02 17:13:07 +0700

    add unit test

--- src/Elect.Core/SimilarityUtils/JaroWinkler.cs
+++ src/Elect.Core/SimilarityUtils/JaroWinkler.cs
@@ -3,28 +3,36 @@ using System.Linq;
 
 namespace Elect.Core.SimilarityUtils
 {
+    /// The Jaro–Winkler distance metric is designed and best suited for short
+    /// strings such as person names, and to detect typos; it is (roughly) a
+    /// variation of Damerau-Levenshtein, where the substitution of 2 close
+    /// characters is considered less important then the substitution of 2 characters
+    /// that a far from each other.
+    /// Jaro-Winkler was developed in the area of record linkage (duplicate
+    /// detection) (Winkler, 1990). It returns a value in the interval [0.0, 1.0].
+    /// The distance is computed as 1 - Jaro-Winkler similarity.
     public class JaroWinkler
     {
-        private const double DEFAULT_THRESHOLD = 0.7;
-        private const int THREE = 3;
-        private const double JW_COEF = 0.1;
+        private const double DefaultThreshold = 0.7;
+        private const int Three = 3;
+        private const double JwCoef = 0.1;
 
         /// <summary>
-        /// The current value of the threshold used for adding the Winkler bonus. The default value is 0.7.
+        ///     The current value of the threshold used for adding the Winkler bonus. The default value is 0.7.
         /// </summary>
         private double Threshold { get; }
-        
+
         /// <summary>
-        /// Creates a new instance with default threshold (0.7)
+        ///     Creates a new instance with default threshold (0.7)
         /// </summary>
         public JaroWinkler()
         {
-            Threshold = DEFAULT_THRESHOLD;
+            Threshold = DefaultThreshold;
         }
-        
+
         /// <summary>
-        /// Creates a new instance with given threshold to determine when Winkler bonus should
-        /// be used. Set threshold to a negative value to get the Jaro distance.
+        ///     Creates a new instance with given threshold to determine when Winkler bonus should
+        ///     be used. Set threshold to a negative value to get the Jaro distance.
         /// </summary>
         /// <param name="threshold"></param>
         public JaroWinkler(double threshold)
@@ -33,7 +41,7 @@ namespace Elect.Core.SimilarityUtils
         }
 
         /// <summary>
-        /// Compute Jaro-Winkler similarity.
+        ///     Compute Jaro-Winkler similarity.
         /// </summary>
         /// <param name="s1">The first string to compare.</param>
         /// <param name="s2">The second string to compare.</param>
@@ -43,7 +51,7 @@ namespace Elect.Core.SimilarityUtils
         {
             if (s1 == null)
             {
-                throw new ArgumentNullException(nameof(s1));    
+                throw new ArgumentNullException(nameof(s1));
             }
 
             if (s2 == null)
@@ -62,26 +70,27 @@ namespace Elect.Core.SimilarityUtils
             {
                 return 0f;
             }
+
             double j = ((m / s1.Length + m / s2.Length + (m - mtp[1]) / m))
-                    / THREE;
+                       / Three;
             double jw = j;
 
             if (j > Threshold)
             {
-                jw = j + Math.Min(JW_COEF, 1.0 / mtp[THREE]) * mtp[2] * (1 - j);
+                jw = j + Math.Min(JwCoef, 1.0 / mtp[Three]) * mtp[2] * (1 - j);
             }
+
             return jw;
         }
 
         /// <summary>
-        /// Return 1 - similarity.
+        ///     Return 1 - similarity.
         /// </summary>
         /// <param name="s1">The first string to compare.</param>
         /// <param name="s2">The second string to compare.</param>
         /// <returns>1 - similarity</returns>
         /// <exception cref="ArgumentNullException">If s1 or s2 is null.</exception>
-        public double Distance(string s1, string s2)
-            => 1.0 - Similarity(s1, s2);
+        public double Distance(string s1, string s2) => 1.0 - Similarity(s1, s2);
 
         private static int[] Matches(string s1, string s2)
         {
@@ -96,47 +105,51 @@ namespace Elect.Core.SimilarityUtils
                 max = s2;
                 min = s1;
             }
+
             int range = Math.Max(max.Length / 2 - 1, 0);
 
-            //int[] matchIndexes = new int[min.Length];
-            //Arrays.fill(matchIndexes, -1);
-            int[] match_indexes = Enumerable.Repeat(-1, min.Length).ToArray();
+            int[] matchIndexes = Enumerable.Repeat(-1, min.Length).ToArray();
 
-            bool[] match_flags = new bool[max.Length];
+            bool[] matchFlags = new bool[max.Length];
             int matches = 0;
             for (int mi = 0; mi < min.Length; mi++)
             {
                 char c1 = min[mi];
                 for (int xi = Math.Max(mi - range, 0),
-                        xn = Math.Min(mi + range + 1, max.Length); xi < xn; xi++)
+                    xn = Math.Min(mi + range + 1, max.Length);
+                    xi < xn;
+                    xi++)
                 {
-                    if (!match_flags[xi] && c1 == max[xi])
+                    if (!matchFlags[xi] && c1 == max[xi])
                     {
-                        match_indexes[mi] = xi;
-                        match_flags[xi] = true;
+                        matchIndexes[mi] = xi;
+                        matchFlags[xi] = true;
                         matches++;
                         break;
                     }
                 }
             }
+
             char[] ms1 = new char[matches];
             char[] ms2 = new char[matches];
             for (int i = 0, si = 0; i < min.Length; i++)
             {
-                if (match_indexes[i] != -1)
+                if (matchIndexes[i] != -1)
                 {
                     ms1[si] = min[i];
                     si++;
                 }
             }
+
             for (int i = 0, si = 0; i < max.Length; i++)
             {
-                if (match_flags[i])
+                if (matchFlags[i])
                 {
                     ms2[si] = max[i];
                     si++;
                 }
             }
+
             int transpositions = 0;
             for (int mi = 0; mi < ms1.Length; mi++)
             {
@@ -145,6 +158,7 @@ namespace Elect.Core.SimilarityUtils
                     transpositions++;
                 }
             }
+
             int prefix = 0;
             for (int mi = 0; mi < min.Length; mi++)
             {
@@ -157,7 +171,8 @@ namespace Elect.Core.SimilarityUtils
                     break;
                 }
             }
-            return new[] { matches, transpositions / 2, prefix, max.Length };
+
+            return new[] {matches, transpositions / 2, prefix, max.Length};
         }
     }
 }
