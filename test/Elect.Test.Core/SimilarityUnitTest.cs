80e768da02d775a18b713b27b489d8d03384c0c6
Top Nguyễn <topnguyen92@gmail.com>
2018-11-02 17:13:07 +0700

    add unit test

--- test/Elect.Test.Core/SimilarityUnitTest.cs
+++ test/Elect.Test.Core/SimilarityUnitTest.cs
@@ -1,5 +1,4 @@
 ﻿using Elect.Core.SimilarityUtils;
-using Elect.Core.StringUtils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Elect.Test.Core
@@ -12,13 +11,13 @@ namespace Elect.Test.Core
         {
             var jw = new JaroWinkler();
 
-            var twitter = jw.Similarity(StringHelper.Normalize("Twitter"), StringHelper.Normalize("twitter"));
-            var chien = jw.Similarity(StringHelper.Normalize("chien"), StringHelper.Normalize("niche"));
-            var twitterv1v2 = jw.Similarity(StringHelper.Normalize("twitter v1"), StringHelper.Normalize("Twitter v2"));
-            var Shazam = jw.Similarity(StringHelper.Normalize("ShazamIphone"), StringHelper.Normalize("ShazamAndroid"));
-            var FamosInstagramSW = jw.Similarity(StringHelper.Normalize("Famos Instagram SW"), StringHelper.Normalize("Famous Instagram"));
-            var IntFacebook1 = jw.Similarity(StringHelper.Normalize("Int Facebook"), StringHelper.Normalize("CI Facebook"));
-            var IntFacebook2 = jw.Similarity(StringHelper.Normalize("Int Facebook"), StringHelper.Normalize("Instagram Int"));
+            var twitter = jw.Similarity("Twitter", "twitter");
+            var chien = jw.Similarity(("chien"), ("niche"));
+            var twitterv1v2 = jw.Similarity("twitter v1", "Twitter v2");
+            var Shazam = jw.Similarity("ShazamIphone", "ShazamAndroid");
+            var FamosInstagramSW = jw.Similarity("Famos Instagram SW", "Famous Instagram");
+            var IntFacebook1 = jw.Similarity("Int Facebook", "CI Facebook");
+            var IntFacebook2 = jw.Similarity("Int Facebook", "Instagram Int");
         }
     }
 }
