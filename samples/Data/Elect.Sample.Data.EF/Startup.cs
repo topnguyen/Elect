a10385a5026f0558eac81aff91df77bb210338c5
Top Nguyen <admin@topnguyen.com>
2018-11-07 15:59:34 +0700

    remove initial by elect

--- samples/Data/Elect.Sample.Data.EF/Startup.cs
+++ samples/Data/Elect.Sample.Data.EF/Startup.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Elect.Web.Middlewares.InitialMiddleware;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,19 +8,13 @@ namespace Elect.Sample.Data.EF
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<Elect.Data.EF.Interfaces.DbContext.IDbContext, Elect.Sample.Data.EF.Services.DbContext>();
-            services.AddScoped(typeof(Elect.Sample.Data.EF.Interfaces.IRepository<>), typeof(Elect.Sample.Data.EF.Services.Repository<>));
-            services.AddScoped<Elect.Sample.Data.EF.Interfaces.IUnitOfWork, Elect.Sample.Data.EF.Services.UnitOfWork>();
+            services.AddScoped<Elect.Data.EF.Interfaces.DbContext.IDbContext, Services.DbContext>();
+            services.AddScoped(typeof(Interfaces.IRepository<>), typeof(Services.Repository<>));
+            services.AddScoped<Interfaces.IUnitOfWork, Services.UnitOfWork>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseElectInitial((serviceProvider) =>
-            {
-                var dbContext = serviceProvider.GetService<Elect.Data.EF.Interfaces.DbContext.IDbContext>();
-                
-                Console.WriteLine("Initial Elect");
-            });
         }
     }
 }
