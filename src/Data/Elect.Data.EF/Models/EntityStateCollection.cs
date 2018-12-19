904c32ece0c7a194aba1623c37e4fd99763ea3fe
Top Nguyen <admin@topnguyen.com>
2018-12-19 23:42:35 +0700

    improve all

---
+++ src/Data/Elect.Data.EF/Models/EntityStateCollection.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Elect.Data.EF.Models
+{
+    public class EntityStateCollection
+    {
+        public List<EntityStateModel> ListAdded { get; set; } = new List<EntityStateModel>();
+        public List<EntityStateModel> ListModified { get; set; } = new List<EntityStateModel>();
+        public List<EntityStateModel> ListDeleted { get; set; } = new List<EntityStateModel>();
+    }
+}
---
+++ src/Data/Elect.Data.EF/Models/EntityStateModel.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Elect.Core.ObjUtils;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Elect.Data.EF.Models
+{
+    public class EntityStateModel
+    {
+        public EntityState State { get; set; }
+
+        public IEnumerable<PropertyEntry> Properties { get; set; }
+
+        public object Entity { get; set; }
+
+        public EntityStateModel()
+        {
+        }
+
+        public EntityStateModel(EntityEntry entityEntry)
+        {
+            State = entityEntry.State;
+            Properties = entityEntry.Properties.Clone();
+            Entity = entityEntry.Entity;
+        }
+    }
+}
