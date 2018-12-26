cd4d19fa712600c6f78d97aa58ab32520eaa7743
Top Nguyen <admin@topnguyen.com>
2018-12-26 17:50:59 +0700

    improve log dashboard

--- src/Logger/Elect.Logger/Logging/Models/ElectLogOptions.cs
+++ src/Logger/Elect.Logger/Logging/Models/ElectLogOptions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Elect.Core.Interfaces;
 using Elect.Logger.Models.Logging;
+using Microsoft.AspNetCore.Http;
 
 namespace Elect.Logger.Logging.Models
 {
@@ -20,23 +22,31 @@ namespace Elect.Logger.Logging.Models
         public bool IsEnableLogToConsole { get; set; } = true;
 
         public bool IsEnableLogToFile { get; set; } = true;
-        
+
         /// <summary>
         ///     Api Document Endpoint, default is "/developers/logs". 
         /// </summary>
         /// <remarks> Must start with "/" </remarks>
         public string Url { get; set; } = "/developers/logs";
-        
+
         /// <summary>
         ///     Access Key via uri param "key", default is "" - allow anonymous. 
         /// </summary>
         public string AccessKey { get; set; } = string.Empty;
-        
+
         /// <summary>
         ///     Un-authorize message when user access api document with not correct key. Default is
         ///     "You don't have permission to view API Document, please contact your administrator."
         /// </summary>
-        public string UnAuthorizeMessage { get; set; } = "You don't have permission to view Develop Logs, please contact your administrator.";
+        public string UnAuthorizeMessage { get; set; } =
+            "You don't have permission to view Develop Logs, please contact your administrator.";
 
+        /// <summary>
+        ///    By default when user access Log Dashboard Url. <br />
+        ///    We support filter log by query strings "skip" (int), "take" (int), "message" (string), "type" (string) <br />
+        ///    Then you can handle more by add Func BeforeLogResponse. 
+        /// </summary>
+        /// <remarks>The query string "full_info" (bool) auto apply before response after this Func</remarks>
+        public Func<HttpContext, IEnumerable<LogModel>, List<LogModel>> BeforeLogResponse { get; set; }
     }
 }
--- src/Logger/Elect.Logger/Utils/LogHelper.cs
+++ src/Logger/Elect.Logger/Utils/LogHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Linq;
 using Elect.Core.LinqUtils;
@@ -18,13 +19,11 @@ namespace Elect.Logger.Utils
 
         public static ContentResult GetLogContentResult(HttpContext context, ElectLogOptions options)
         {
-            ContentResult contentResult;
-
             var requestPath = context.Request.Path.Value.Trim('/');
 
             var summaryUrl = options.Url.Trim('/');
 
-            contentResult = requestPath == summaryUrl
+            var contentResult = requestPath == summaryUrl
                 ? GetSummaryContentResult(context, options)
                 : GetDetailContentResult(context, options);
 
@@ -40,8 +39,8 @@ namespace Elect.Logger.Utils
                 return new ContentResult
                 {
                     ContentType = ContentType.Json,
-                    StatusCode = StatusCodes.Status204NoContent,
-                    Content = string.Empty
+                    StatusCode = StatusCodes.Status200OK,
+                    Content = "{}"
                 };
             }
 
@@ -92,6 +91,12 @@ namespace Elect.Logger.Utils
 
         private static ContentResult GetDetailContentResult(HttpContext context, ElectLogOptions options)
         {
+            // Get Log File
+
+            var requestPath = context.Request.Path.Value.Trim('/');
+
+            var lastPath = requestPath.Split('/').LastOrDefault();
+
             var storageFolder = Path.GetDirectoryName(Path.GetFullPath(options.JsonFilePath));
 
             if (string.IsNullOrWhiteSpace(storageFolder))
@@ -99,16 +104,65 @@ namespace Elect.Logger.Utils
                 return new ContentResult
                 {
                     ContentType = ContentType.Json,
-                    StatusCode = StatusCodes.Status204NoContent,
-                    Content = string.Empty
+                    StatusCode = StatusCodes.Status200OK,
+                    Content = "{}"
                 };
             }
 
-            var requestPath = context.Request.Path.Value.Trim('/');
-
-            var lastPath = requestPath.Split('/').LastOrDefault();
-
             var logFilePath = Directory.GetFiles(storageFolder).FirstOrDefault(x => Path.GetFileName(x) == lastPath);
+
+            if (string.IsNullOrWhiteSpace(logFilePath))
+            {
+                return new ContentResult
+                {
+                    ContentType = ContentType.Json,
+                    StatusCode = StatusCodes.Status200OK,
+                    Content = "{}"
+                };
+            }
+
+            // Skip
+            int skip = 0;
+            if (context.Request.Query.TryGetValue("skip", out var skipStr))
+            {
+                if (int.TryParse(skipStr, out var skipInt))
+                {
+                    skip = skipInt;
+                }
+            }
+
+            // Take
+            int take = 1000;
+            if (context.Request.Query.TryGetValue("take", out var takeStr))
+            {
+                if (int.TryParse(takeStr, out var takeInt))
+                {
+                    take = takeInt;
+                }
+            }
+
+            // Full Info or not
+            bool isFullInfo = false;
+            if (context.Request.Query.TryGetValue("full_info", out var fullInfoStr))
+            {
+                if (bool.TryParse(fullInfoStr, out var fullInfoBool))
+                {
+                    isFullInfo = fullInfoBool;
+                }
+            }
+
+            // Message Filter
+            context.Request.Query.TryGetValue("message", out var message);
+
+            // Log Type Filter
+            LogType? logType = null;
+            if (context.Request.Query.TryGetValue("type", out var type))
+            {
+                if (Enum.TryParse(type, out LogType logTypeEnum))
+                {
+                    logType = logTypeEnum;
+                }
+            }
 
             using (var file = new DataStore(logFilePath))
             {
@@ -119,17 +173,51 @@ namespace Elect.Logger.Utils
                     return new ContentResult
                     {
                         ContentType = ContentType.Json,
-                        StatusCode = StatusCodes.Status204NoContent,
-                        Content = string.Empty
+                        StatusCode = StatusCodes.Status200OK,
+                        Content = "{}"
                     };
                 }
 
-                var logs = file.GetCollection("logs").AsQueryable().ToList(10000);
+                var logs = file.GetCollection<LogModel>("logs").AsQueryable().Skip(skip).Take(take);
 
+                // Filter by Type
+
+                if (logType != null)
+                {
+                    logs = logs.Where(x => x.Type == logType);
+                }
+
+                // Filter by Message
+
+                if (!string.IsNullOrWhiteSpace(message))
+                {
+                    logs = logs.Where(x =>
+                        !string.IsNullOrWhiteSpace(x.Message) && x.Message.Contains(message) ||
+                        message.Contains(x.Message));
+                }
+
+                if (options.BeforeLogResponse != null)
+                {
+                    logs = options.BeforeLogResponse(context, logs);
+                }
+
+                var resultLogs = logs.ToList(10000);
+
+                if (!isFullInfo)
+                {
+                    foreach (var logModel in resultLogs)
+                    {
+                        logModel.HttpContext = null;
+                        logModel.Runtime = null;
+                        logModel.EnvironmentModel = null;
+                        logModel.Sdk = null;
+                    }
+                }
+                
                 var content = new
                 {
                     meta,
-                    logs
+                    logs = resultLogs
                 }.ToJsonString();
 
                 ContentResult contentResult = new ContentResult
