1477011517d0cbc606c80a589aa5babe591b20db
Top Nguyen <topnguyen92@gmail.com>
2018-12-26 13:29:56 +0700

    update libs

---
+++ test/Elect.Test.Core/MixUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Elect.Core.LinqUtils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Elect.Test.Core
+{
+    [TestClass]
+    public class MixUnitTest
+    {
+        [TestMethod]
+        public void MixCase()
+        {
+            var items = new List<int> {1, 5, 7, 3, 10, 9, 6};
+
+            var wherePrevious = items.WherePrevious((first, second) => second > first).ToList();
+
+            var randomize = items.Randomize().ToList();
+
+            var l = items.ToList();
+        }
+    }
+}
