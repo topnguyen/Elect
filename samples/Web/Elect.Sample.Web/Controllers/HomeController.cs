8673e0ddd00358bad92a27300fbf3b55491a8069
Top Nguyen <topnguyen92@gmail.com>
2018-12-26 13:45:51 +0700

    update libs

--- samples/Web/Elect.Sample.Web/Controllers/HomeController.cs
+++ samples/Web/Elect.Sample.Web/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using Elect.Logger.Logging;
-using Elect.Web.SiteMap.Attributes;
+﻿using Elect.Web.SiteMap.Attributes;
 using Elect.Web.SiteMap.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,23 +6,9 @@ namespace Elect.Sample.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IElectLog _electLog;
-
-        public HomeController(IElectLog electLog)
-        {
-            _electLog = electLog;
-        }
-        
         [SiteMapItem(SiteMapItemFrequency.Hourly, 0.9)]
         public IActionResult Index()
         {
-            _electLog.BeforeLog = (logInfo) =>
-            {
-                var log = logInfo;
-                return logInfo;
-            };
-            
-            _electLog.Capture("message credit cart is 378282246310005");
             return View();
         }
 
