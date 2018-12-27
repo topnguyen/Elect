cf3b6f2ac09007ab811ac6524a31a9dc88b9b29a
Top Nguyen <admin@topnguyen.com>
2018-12-27 16:43:54 +0700

    correct info

--- samples/DI/Elect.Sample.DI/Startup.cs
+++ samples/DI/Elect.Sample.DI/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Elect.Core.StringUtils;
+﻿using System.Collections.Generic;
+using Elect.Core.StringUtils;
 using Elect.DI;
 using Elect.DI.Attributes;
 using Microsoft.AspNetCore.Builder;
@@ -13,6 +14,18 @@ namespace Elect.Sample.DI
         {
             // Auto Register Dependency Injection
             services.AddElectDI();
+            
+//          services.AddElectDI(_ =>
+//          {
+//              _.ListAssemblyName = new List<string> // default is 1 assembly name: application name - Elect.Sample.DI
+//              {
+//                  "ExampleAssembly" // will scan ExampleAssembly.dll and ExampleAssembly.*.dll
+//              };
+//              _.ListAssemblyFolderPath = new List<string> // default is  1 folder: application base folder path
+//              {
+//                  "C:\\ExampleAssemblyFolderPath" 
+//              };
+//          });
 
             // Optional - Print out Registered Service with Information
             services.PrintServiceAddedToConsole();
