56999374f0a854a6492014862f6c8186120f3930
Top Nguyen <admin@topnguyen.com>
2019-01-13 01:21:28 +0700

    correct datetime parse

--- test/Elect.Test.Core/MixUnitTest.cs
+++ test/Elect.Test.Core/MixUnitTest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Elect.Core.LinqUtils;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Elect.Test.Core
 {
@@ -12,13 +8,6 @@ namespace Elect.Test.Core
         [TestMethod]
         public void MixCase()
         {
-            var items = new List<int> {1, 5, 7, 3, 10, 9, 6};
-
-            var wherePrevious = items.WherePrevious((first, second) => second > first).ToList();
-
-            var randomize = items.Randomize().ToList();
-
-            var l = items.ToList();
         }
     }
 }
