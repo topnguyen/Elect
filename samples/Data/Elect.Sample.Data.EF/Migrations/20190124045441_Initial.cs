e54d7f07c966e233701ecb2d0a9a0c2c0b79442e
Top Nguyen <topnguyen92@gmail.com>
2019-01-24 17:46:18 +0700

    migrate

---
+++ samples/Data/Elect.Sample.Data.EF/Migrations/20190124045441_Initial.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Elect.Sample.Data.EF.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    LastUpdatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    DeletedTime = table.Column<DateTimeOffset>(nullable: true),
+                    CreatedBy = table.Column<Guid>(nullable: true),
+                    LastUpdatedBy = table.Column<Guid>(nullable: true),
+                    DeletedBy = table.Column<Guid>(nullable: true),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserProfile",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    LastUpdatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    DeletedTime = table.Column<DateTimeOffset>(nullable: true),
+                    CreatedBy = table.Column<Guid>(nullable: true),
+                    LastUpdatedBy = table.Column<Guid>(nullable: true),
+                    DeletedBy = table.Column<Guid>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfile", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserProfile_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_DeletedTime",
+                table: "User",
+                column: "DeletedTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Id",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_UserName",
+                table: "User",
+                column: "UserName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfile_UserId",
+                table: "UserProfile",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserProfile");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
