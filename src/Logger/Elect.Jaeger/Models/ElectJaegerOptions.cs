52f6156588d861ed0247d5158e254e5cdaa654fb
Top Nguyen <admin@topnguyen.com>
2019-05-12 00:04:54 +0700

    Add Elect Jaeger

---
+++ src/Logger/Elect.Jaeger/Models/ElectJaegerOptions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Elect.Core.Interfaces;
+using Jaeger;
+using Microsoft.Extensions.PlatformAbstractions;
+using OpenTracing;
+
+namespace Elect.Jaeger.Models
+{
+    /// <summary>
+    ///     Config for the Jaeger Client, see more https://github.com/jaegertracing/jaeger-client-csharp
+    /// </summary>
+    public class ElectJaegerOptions : IElectOptions
+    {
+        /// <summary>
+        ///     Service Name, default is the Application Name
+        /// </summary>
+        public string ServiceName { get; set; } = PlatformServices.Default.Application.ApplicationName;
+        
+        /// <summary>
+        ///     Default is localhost
+        /// </summary>
+        public string Domain { get; set; } = "localhost";
+
+        /// <summary>
+        ///     Default is 5778
+        /// </summary>
+        public int SamplerPort { get; set; } = 5778;
+
+        /// <summary>
+        ///     Default is 6831
+        /// </summary>
+        public int ReporterPort { get; set; } = 6831;
+
+        /// <summary>
+        ///     Callback after finish setup SamplerConfiguration, allow you adjust setup for SamplerConfiguration
+        /// </summary>
+        /// <remarks>If the result of your callback is <c>null</c> then stop add Jaeger Service</remarks>
+        public Func<Configuration.SamplerConfiguration, Configuration.SamplerConfiguration> AfterSamplerConfig { get; set; }
+        
+        /// <summary>
+        ///     Callback after finish setup SamplerConfiguration, allow you adjust setup for SamplerConfiguration
+        /// </summary>
+        /// <remarks>If the result of your callback is <c>null</c> then stop add Jaeger Service</remarks>
+        public Func<Configuration.ReporterConfiguration, Configuration.ReporterConfiguration> AfterReporterConfig { get; set; }
+        
+        /// <summary>
+        ///     Callback after finish setup Configuration, allow you adjust setup for Configuration
+        /// </summary>
+        /// <remarks>If the result of your callback is <c>null</c> then stop add Jaeger Service</remarks>
+        public Func<Configuration, Configuration> AfterGlobalConfig { get; set; }
+        
+        /// <summary>
+        ///     Callback after finish config Tracer, allow you adjust config for Tracer
+        /// </summary>
+        public Func<ITracer, ITracer> AfterTracer { get; set; }
+    }
+}
---
+++ src/Logger/Elect.Jaeger/README.md
@@ -0,0 +1,27 @@
+﻿![Logo](../../../Logo.png)
+# Elect.Jaeger
+> Author [**Top Nguyen**](http://topnguyen.net)
+
+## Overview
+
+## Installation
+- Package Manager
+```
+PM> Install-Package Elect.Jaeger
+```
+- .NET CLI
+```
+dotnet add package Elect.Jaeger
+```
+
+See more information in [Nuget Package](https://www.nuget.org/packages/Elect.Jaeger/).
+
+## Usage
+
+- Add Service
+
+```c#
+services.AddElectJaeger();
+```
+## License
+Elect.Jaeger is licensed under the [MIT License](../../../LICENSE).
