4350cb970201b10d25c382ed5439a4f7f1c240cf
Top Nguyen <topnguyen92@gmail.com>
2019-05-31 20:04:24 +0700

    remove middle ware

--- samples/Web/Elect.Sample.Web/Startup.cs
+++ samples/Web/Elect.Sample.Web/Startup.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Linq;
 using Elect.Logger.Logging;
-using Elect.Web.Middlewares.ReverseProxyMiddleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Elect.Sample.Web
@@ -13,15 +11,6 @@ namespace Elect.Sample.Web
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddElectReserveProxy(_ =>
-            //{
-            //    _.ServiceRootUrl = "http://127.0.0.1:8080/";
-            //    _.AfterReserveProxy = context =>
-            //    {
-            //        Console.WriteLine("URL " + context.Request.GetDisplayUrl());
-            //    };
-            //});
-
             services.AddElectLog(_ =>
             {
                 _.BeforeLogResponse = (context, models) =>
