2219fe8d5837101cf65f74dbb9f80a7ab55476fa
Top Nguyen <admin@topnguyen.com>
2019-06-16 14:56:36 +0700

    update libs

--- src/Web/Elect.Web/Api/Models/PagedResponseModel{T}.cs
+++ src/Web/Elect.Web/Api/Models/PagedResponseModel{T}.cs
@@ -26,16 +26,16 @@ namespace Elect.Web.Api.Models
     public class PagedResponseModel<T>: ElectDisposableModel where T : class, new()
     {
         [JsonProperty(Order = 6)]
-        public int Total { get; set; }
+        public virtual int Total { get; set; }
 
         [JsonProperty(Order = 7)]
-        public IEnumerable<T> Items { get; set; }
+        public virtual IEnumerable<T> Items { get; set; }
 
         /// <summary>
         ///     Will be de-serialize as list property 
         /// </summary>
         [JsonProperty(Order = 8)]
         [JsonExtensionData]
-        public Dictionary<string, object> AdditionalData { get; set; } = new Dictionary<string, object>();
+        public virtual Dictionary<string, object> AdditionalData { get; set; } = new Dictionary<string, object>();
     }
 }
