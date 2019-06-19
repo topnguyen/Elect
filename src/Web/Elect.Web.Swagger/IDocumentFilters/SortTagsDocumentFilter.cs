9974aafb5c36a6cee527aaffb82715d409435370
Top Nguyen <admin@topnguyen.com>
2019-06-19 23:15:12 +0700

    correct swagger config

---
+++ src/Web/Elect.Web.Swagger/IDocumentFilters/SortTagsDocumentFilter.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Elect.Web.Swagger.IDocumentFilters
+{
+    public class SortTagsDocumentFilter : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
+        {
+            swaggerDoc.Tags = swaggerDoc.Tags.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
