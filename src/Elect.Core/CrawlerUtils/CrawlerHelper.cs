2219267f4ca7fa4a135f04fe8449578ae3cd83d0
Top Nguyen <admin@topnguyen.com>
2020-06-17 10:59:16 +0700

    update metadata method

--- src/Elect.Core/CrawlerUtils/CrawlerHelper.cs
+++ src/Elect.Core/CrawlerUtils/CrawlerHelper.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
 using AngleSharp.Io.Network;
 using Elect.Core.CrawlerUtils.Models;
 
@@ -23,7 +25,7 @@ namespace Elect.Core.CrawlerUtils
 
             foreach (var url in urls)
             {
-                Task<MetadataModel> task = GetMetadataAsync(url);
+                Task<MetadataModel> task = GetMetadataByUrlAsync(url);
 
                 tasks.Add(task);
             }
@@ -33,31 +35,30 @@ namespace Elect.Core.CrawlerUtils
             return metadataModels.ToList();
         }
 
-        public static async Task<MetadataModel> GetMetadataAsync(string url)
+        public static async Task<MetadataModel> GetMetadataByUrlAsync(string url)
         {
-            var metadataModel = new MetadataModel
-            {
-                Url = url
-            };
-
             try
             {
                 var httpClient = new HttpClient();
+
+                // Re-procedure as Native browser
                 
                 httpClient
                     .DefaultRequestHeaders
-                    .Add("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36");
-                    
+                    .Add("user-agent",
+                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36");
+
                 httpClient
                     .DefaultRequestHeaders
-                    .Add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-                      
+                    .Add("accept",
+                        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+
                 httpClient
                     .DefaultRequestHeaders
                     .Add("upgrade-insecure-requests", "1");
-                
+
                 var requester = new HttpClientRequester(httpClient);
-                
+
                 var browsingConfig = Configuration
                     .Default
                     .WithRequester(requester)
@@ -66,119 +67,137 @@ namespace Elect.Core.CrawlerUtils
 
                 using var browsingContext = BrowsingContext.New(browsingConfig);
 
-                var document = await browsingContext.OpenAsync(url);
+                var htmlDocument = await browsingContext.OpenAsync(url);
 
-                if (document == null)
-                {
-                    return metadataModel;
-                }
-
-                var metaTags = document.GetElementsByTagName("meta");
-
-                foreach (var metaTag in metaTags)
-                {
-                    var metaTagModel = new MetaTagModel
-                    {
-                        Html = metaTag.OuterHtml,
-                        Attributes = metaTag.Attributes.ToDictionary(x => x.Name, x => x.Value)
-                    };
-
-                    metadataModel.MetaTags.Add(metaTagModel);
-
-                    // Check Property Value
-
-                    var propertyValue =
-                        metaTag
-                            .Attributes
-                            .FirstOrDefault(x => x.Name.ToLowerInvariant() == "property")
-                            ?.Value;
-
-                    // Check Name Value
-
-                    var nameValue =
-                        metaTag
-                            .Attributes
-                            .FirstOrDefault(x => x.Name.ToLowerInvariant() == "name")
-                            ?.Value;
-
-                    if (string.IsNullOrWhiteSpace(propertyValue) && string.IsNullOrWhiteSpace(nameValue))
-                    {
-                        continue;
-                    }
-
-                    var contentValue = metaTag
-                        .Attributes
-                        .FirstOrDefault(x => x.Name.ToLowerInvariant() == "content")
-                        ?.Value;
-
-                    switch (propertyValue ?? nameValue)
-                    {
-                        case "og:url":
-                        {
-                            metadataModel.Url = contentValue;
-                            break;
-                        }
-                        case "og:title":
-                        {
-                            metadataModel.Title = contentValue;
-                            break;
-                        }
-                        case "og:description":
-                        {
-                            metadataModel.Description = contentValue;
-                            break;
-                        }
-                        case "og:image":
-                        {
-                            metadataModel.Image = contentValue;
-                            break;
-                        }
-                        case "og:video":
-                        {
-                            metadataModel.Image = contentValue;
-                            break;
-                        }
-                        case "og:type":
-                        {
-                            metadataModel.Type = contentValue;
-                            break;
-                        }
-                        case "og:locale":
-                        {
-                            metadataModel.Locale = contentValue;
-                            break;
-                        }
-                        case "og:site_name":
-                        {
-                            metadataModel.SiteName = contentValue;
-                            break;
-                        }
-                        case "author":
-                        {
-                            metadataModel.Author = contentValue;
-                            break;
-                        }
-                    }
-                }
-
-                metadataModel.Title = string.IsNullOrWhiteSpace(metadataModel.Title)
-                    ? document.Title
-                    : metadataModel.Title;
-
-                metadataModel.Url = string.IsNullOrWhiteSpace(metadataModel.Url)
-                    ? document.Url
-                    : metadataModel.Url;
+                return GetMetadata(htmlDocument);
             }
             catch (Exception e)
             {
                 // Ignore
-
-#if DEBUG
-
-                Console.WriteLine(e);
-
-#endif
+                #if DEBUG
+                   Console.WriteLine(e);
+                #endif
+                
+                return new MetadataModel
+                {
+                    Url = url
+                };
             }
+        }
+
+        public static async Task<MetadataModel> GetMetadataByHtmlAsync(string html)
+        {
+            var htmlParser = new HtmlParser();
+
+            var htmlDocument = await htmlParser.ParseDocumentAsync(html);
+
+            return GetMetadata(htmlDocument);
+        }
+        
+        private static MetadataModel GetMetadata(IDocument htmlDocument)
+        {
+            var metadataModel = new MetadataModel();
+
+            if (htmlDocument == null)
+            {
+                return metadataModel;
+            }
+
+            var metaTags = htmlDocument.GetElementsByTagName("meta");
+
+            foreach (var metaTag in metaTags)
+            {
+                var metaTagModel = new MetaTagModel
+                {
+                    Html = metaTag.OuterHtml,
+                    Attributes = metaTag.Attributes.ToDictionary(x => x.Name, x => x.Value)
+                };
+
+                metadataModel.MetaTags.Add(metaTagModel);
+
+                // Check Property Value
+
+                var propertyValue =
+                    metaTag
+                        .Attributes
+                        .FirstOrDefault(x => x.Name.ToLowerInvariant() == "property")
+                        ?.Value;
+
+                // Check Name Value
+
+                var nameValue =
+                    metaTag
+                        .Attributes
+                        .FirstOrDefault(x => x.Name.ToLowerInvariant() == "name")
+                        ?.Value;
+
+                if (string.IsNullOrWhiteSpace(propertyValue) && string.IsNullOrWhiteSpace(nameValue))
+                {
+                    continue;
+                }
+
+                var contentValue = metaTag
+                    .Attributes
+                    .FirstOrDefault(x => x.Name.ToLowerInvariant() == "content")
+                    ?.Value;
+
+                switch (propertyValue ?? nameValue)
+                {
+                    case "og:url":
+                    {
+                        metadataModel.Url = contentValue;
+                        break;
+                    }
+                    case "og:title":
+                    {
+                        metadataModel.Title = contentValue;
+                        break;
+                    }
+                    case "og:description":
+                    {
+                        metadataModel.Description = contentValue;
+                        break;
+                    }
+                    case "og:image":
+                    {
+                        metadataModel.Image = contentValue;
+                        break;
+                    }
+                    case "og:video":
+                    {
+                        metadataModel.Image = contentValue;
+                        break;
+                    }
+                    case "og:type":
+                    {
+                        metadataModel.Type = contentValue;
+                        break;
+                    }
+                    case "og:locale":
+                    {
+                        metadataModel.Locale = contentValue;
+                        break;
+                    }
+                    case "og:site_name":
+                    {
+                        metadataModel.SiteName = contentValue;
+                        break;
+                    }
+                    case "author":
+                    {
+                        metadataModel.Author = contentValue;
+                        break;
+                    }
+                }
+            }
+
+            metadataModel.Title = string.IsNullOrWhiteSpace(metadataModel.Title)
+                ? htmlDocument.Title
+                : metadataModel.Title;
+
+            metadataModel.Url = string.IsNullOrWhiteSpace(metadataModel.Url)
+                ? htmlDocument.Url
+                : metadataModel.Url;
 
             return metadataModel;
         }
