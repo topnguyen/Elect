6c9cfb5891423bb1858a96848cd989c2024e9958
Top Nguyen <admin@topnguyen.com>
2020-06-19 14:53:06 +0700

    update test cases

---
+++ test/Data/Elect.Test.Data.IO/MixUnitTest.cs
@@ -0,0 +1,45 @@
+using Elect.Data.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Elect.Test.Data.IO
+{
+    [TestClass]
+    public class PathUnitTest
+    {
+        [TestMethod]
+        public void GetFullPathTestCase()
+        {
+            var pathRelative = "";
+
+            var fullPath = PathHelper.GetFullPath(pathRelative);
+
+            Assert.IsNotNull(fullPath);
+        }
+
+        [TestMethod]
+        public void CompressTestCase()
+        {
+            // Test On Linux -------------------------
+
+            // var input2Path = "/Users/top/Downloads/Test File 2.png";
+            // var input3Path = "/Users/top/Downloads/Test File 3.JPG";
+            // var input4Path = "/Users/top/Downloads/Test File 4.JPG";
+            // var input5Path = "/Users/top/Downloads/Test File 5.gif";  
+            
+            // ImageCompressor.Compress(input2Path, "/Users/top/Downloads/Test File 2 Compressed.png");
+            // ImageCompressor.Compress(input3Path, "/Users/top/Downloads/Test File 3 Compressed.JPG");
+            // ImageCompressor.Compress(input5Path, "/Users/top/Downloads/Test File 5 Compressed.gif");  
+            
+            // Test On Windows -------------------------
+            
+            // var input2Path = "D:\\Test File 2.png";
+            // var input3Path = "D:\\Test File 3.JPG";
+            // var input4Path = "D:\\Test File 4.JPG";
+            // var input5Path = "D:\\Test File 5.gif";
+            //
+            // ImageCompressor.Compress(input2Path, "D:\\Test File 2 Compressed.png");
+            // ImageCompressor.Compress(input3Path, "D:\\Test File 3 Compressed.JPG");
+            // ImageCompressor.Compress(input5Path, "D:\\Test File 5 Compressed.gif");
+        }
+    }
+}
